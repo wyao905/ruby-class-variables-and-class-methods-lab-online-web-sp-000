a793808d6c3d2e4b4bed4b90bca67069ebd3d615
Warren Yao <warrenyca905@gmail.com>
2019-05-28 00:24:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,6 +3,7 @@ class Song
   
   @@count = 0
   @@genres = []
+  @@artists = []
   
   def initialize(name, artist, genre)
     @name = name
@@ -10,15 +11,29 @@ class Song
     @genre = genre
     @@count += 1
     @@genres << genre
+    @@artists << artist
   end
   
   def self.genres
     list = []
+    @@genres.each {|genre| list << genre if !list.include?(genre)}
+    return list
+  end
+  
+  def self.artists
+    list = []
+    @@artists.each {|artist| list << artist if !list.include?(artist)}
+    return list
+  end
+  
+  def self.genre_count
+    count = {}
     @@genres.each do |genre|
-      if !list.include?(genre)
-        list << genre
+      if count.values.include?(genre)
+        count[genre] += 1
+      else
+        count[genre] = 1
       end
     end
-    return list
   end
 end
